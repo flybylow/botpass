f97778b0320f92107b2f08ed234cf20852a81514
flybylow <warddem@gmail.com>
2025-03-16 23:06:37 +0100

    Fix: Remove unused Link import from WebhookManager

--- src/components/WebhookManager.tsx
+++ src/components/WebhookManager.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useBot } from '@/contexts/BotContext';
 import { useWebhook } from '@/contexts/WebhookContext';
 import { useMessage } from '@/contexts/MessageContext';
